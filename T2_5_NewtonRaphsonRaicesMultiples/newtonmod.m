a64660fab0cb41d1eeb23f4a69e16bd64a5fdaa9
Alf-caput <alfre.roble.aba@gmail.com>
2023-02-26 17:01:09 +0100

    FIN

--- T2_5_NewtonRaphsonRaicesMultiples/newtonmod.m
+++ T2_5_NewtonRaphsonRaicesMultiples/newtonmod.m
@@ -2,9 +2,8 @@ function [xs, i] = newtonmod (f, xest, ermax)
 % Función que permite calcular de manera aproximada una raíz de una función 
 % dado un punto a próximo a ella.
 % El algoritmo utilizado es el Método de Newton-Rapson, no obstante se ha
-% modificado definiendo una nueva función que es el cociente de la
-% función original entre su derivada y que tendrá las mismas raices que la
-% función de entrada.
+% modificado definiendo una nueva función (cociente de la función original 
+% y su derivada) que tendrá las mismas raices que la función de entrada.
 % Inputs: 
 % f = funcion simbólica
 % xest = punto próximo a la raíz
@@ -22,15 +21,14 @@ function [xs, i] = newtonmod (f, xest, ermax)
     df1 = matlabFunction(df1);
     df2 = matlabFunction(df2);
     % Guardamos en variables las imágenes por legibilidad y evitar repetir
-    % llamadas en la expresión de la solución
+    % llamadas en la expresión de la nueva solución
     fxest = f(xest);
     df1xest = df1(xest);
     df2xest = df2(xest);
     % La nueva solución aproximada se calculará con la siguiente expresión
     xs = xest - fxest * df1xest / (df1xest^2 - fxest * df2xest);
-
     i = 1;
-    % Mientras la diferencia relativa de iteraciones sea menor que ermax
+    % Mientras la diferencia relativa entre iteraciones sea menor que ermax
     % o la imagen de la función sea distinta de cero se repite el proceso
     while abs((xs - xest) / xest) > ermax && f(xs) ~= 0
         xest = xs;
@@ -40,7 +38,6 @@ function [xs, i] = newtonmod (f, xest, ermax)
         df2xest = df2(xest);
 
         xs = xest - fxest * df1xest / (df1xest^2 - fxest * df2xest);
-
         i = i+1;
     end
 end
