73c545784584ed3ed72b3a62660331f8e4a7607d
Alf-caput <alfre.roble.aba@gmail.com>
2023-02-27 16:51:08 +0100

    not finished

--- ridder.m
+++ ridder.m
@@ -8,37 +8,39 @@ function [r, i] = ridder(fun, x1, x2, tol)
 % r = raíz encontrada
 % Q = constante que se determina requiriendo que los puntos 
 % (x1, g1), (x2, g2) y (x3,g3) se encuentren en una línea recta
-    x3 = (x1-x2)/2;
-    ehq = @(x) (fun(x3) + sqrt(fun(x3)^2-fun(x1)*fun(x2))) / fun(x2);
-    g  = @(x) fun(x) * ehq;
-    i = 1;
-    while abs(x2 - x1) > tol 
-        i = i + 1;
-        x3 = (x1-x2)/2;
-        if fun(x1) - fun(x2) > 0
+    f1 = fun(x1); f2 = fun(x2)
+    if f1 * f2 < 0
+        x3 = (x1+x2)/2;
+        f3 = fun(x3);
+        if f1 - f2 > 0
             j = 0;
         else
             j = 1;
         end
-    
-        x4 = x3 + (-1)^j*(x3-x1)*fun(x3)/sqrt(fun(x3)^2-fun(x1)*fun(x2));
-    
-        if g(x3) == (g(x1)+g(x2))/2
-            d1 = abs(x4-x1);
-            d2 = abs(x4-x2);
-            d3 = abs(x4-x3);
-            d = [d1, d2, d3];
+        x4 = x3 + (-1)^j * (x3 - x1) * f3 / (sqrt(f3^2 - f1*f2));
+        i = 1;
+        while abs(x4 - x3) > tol
+            [~, ix] = min([abs(x4-x1), abs(x4-x2), abs(x4-x3)]);
+            x = [x1, x2, x3];
+            x2 = x(ix);
+            
+            find(x )
+
             x1 = x4;
-            if max(d) == d1
-                x2 = x1;
-            elseif max(d) == d3
-                x2 = x3;
+            x3 = (x1+x2)/2;
+            if f1 - f2 > 0
+                j = 0;
+            else
+                j = 1;
             end
-        else
-            break;
+            x4 = x3 + (-1)^j * (x3 - x1) * f3 / (sqrt(f3^2 - f1*f2));
+
+            i = i + 1;
         end
+        r = x4;
+    else
+        disp("Este intervalo no contiene raices.")
     end
-    r = x4;
 end
 
 
