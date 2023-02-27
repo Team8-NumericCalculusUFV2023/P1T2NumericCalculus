35219f8ab08f4922af8fdc2350b4ceab525ecf7e
Alf-caput <alfre.roble.aba@gmail.com>
2023-02-27 17:30:26 +0100

    FIN

--- ridder.m
+++ ridder.m
@@ -20,17 +20,21 @@ function [r, i] = ridder(fun, x1, x2, tol)
         end
         x4 = x3 + (-1)^j * (x3 - x1) * f3 / (sqrt(f3^2 - f1*f2));
         i = 1;
-        while abs(x4 - x3) > tol
+        while abs(x4 - x3) > tol && f(x4) ~= 0
             x = [x1, x2, x3];
             [~, ix] = min(abs(x4-x));
             x2 = x(ix);
             x1 = x4;
             x3 = (x1+x2)/2;
+
             if f1 - f2 > 0
                 j = 0;
             else
                 j = 1;
             end
+            f1 = fun(x1); 
+            f2 = fun(x2);
+            f3 = fun(x3);
             x4 = x3 + (-1)^j * (x3 - x1) * f3 / (sqrt(f3^2 - f1*f2));
             i = i + 1;
         end
