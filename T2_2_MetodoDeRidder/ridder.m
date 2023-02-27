646a3e1b75bfa3294498efbd1eabd8fd4c9a7d44
anruki <79452048+anruki@users.noreply.github.com>
2023-02-27 20:39:20 +0100

    Update ridder.m
    fixed error in naming the function as f instead of fun

--- T2_2_MetodoDeRidder/ridder.m
+++ T2_2_MetodoDeRidder/ridder.m
@@ -10,7 +10,7 @@ function [r, i] = ridder(fun, x1, x2, tol)
 % (x1, g1), (x2, g2) y (x3,g3) se encuentren en una línea recta
     f1 = fun(x1); 
     f2 = fun(x2);
-    if f1 * f2 < 0
+    if f1 * f2 < 0      % BOLZANO (comprobamos que haya al menos una raiz)
         x3 = (x1+x2)/2;
         f3 = fun(x3);
         if f1 - f2 > 0
@@ -20,7 +20,7 @@ function [r, i] = ridder(fun, x1, x2, tol)
         end
         x4 = x3 + (-1)^j * (x3 - x1) * f3 / (sqrt(f3^2 - f1*f2));
         i = 1;
-        while abs(x4 - x3) > tol && f(x4) ~= 0
+        while abs(x4 - x3) > tol && fun(x4) ~= 0
             x = [x1, x2, x3];
             [~, ix] = min(abs(x4-x));
             x2 = x(ix);
