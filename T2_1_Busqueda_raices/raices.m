6f1ca9aa20d74c5a8b75377b360d373abafbfc8e
Alf-caput <alfre.roble.aba@gmail.com>
2023-02-27 17:54:34 +0100

    Merge branch 'T2_2_Ridder' into main

--- T2_1_Busqueda_raices/raices.m
+++ T2_1_Busqueda_raices/raices.m
@@ -1,12 +1,18 @@
 function x = raices(fun, a, b, dx)
-
 % Se asume: fun continua en (a, b); a < b; dx <= b-a
     x = [];
+    % Se asegura que está dentro del intervalo
     while a+dx <= b
+        % Al usar la función raizbus se encuentra el intervalo más próximo
+        % a la raíz
         [x1, x2] = raizbus(fun, a, a+dx, dx);
-        x(end+1, :) = [x1, x2]; % No conocemos cuantas hay => redimensionar
-        a = a+dx;
+        % Se guardan los límites en la matriz 
+        x(end+1, :) = [x1, x2];% No conocemos cuantas hay => redimensionar
+        % 'a' avanza hacia b
+        a = a+dx;        
     end
+    % En caso de sobrepasar 'a' a 'b' se comprueba el último 
+    % intervalo (a,b) si hay una raíz
     if a~=b
         [x1, x2] = raizbus(fun, a, a+dx, dx);
         x(end+1, :) = [x1, x2];
--- T2_1_Busqueda_raices/raizbus.m
+++ T2_1_Busqueda_raices/raizbus.m
@@ -1,21 +1,32 @@
 function [x1, x2] = raizbus(fun, a, b, dx)
-% fun = función en forma anónima fun = @(x)
-% (a, b) = intervalo de inicio de búsqueda
-% dx = ancho del intervalo
-% [x1, x2] = intervalo de ancho dx donde se encuentra una raíz de �(�)
 % Se asume: fun continua en (a, b); a < b; dx <= b-a
-    if fun(a) * fun(b) < 0
+% Se usa el teorema de Bolzano para saber si existe una raíz en el
+% intervalo
+    if fun(a) * fun(b) <= 0        
+        % Se asignan los límites del nuevo intervalo dado que se sabe que
+        % en el intervalo (a,b) existe una raíz, se aplica el teorema de
+        % Bolzano a pequeños subintervalos consecutivos desde 'a' para
+        % encontrar el intervalo más pequeño que contenga a la raíz.
         [x1, x2] = deal(a, a+dx);
+        % Con esta condición se asegura que x2 permanece en el intervalo
+        % original     
         while x2 <= b 
-            if fun(x1) * fun(x2) < 0
+            % Si el subintervalo se encuentra la raíz se sale del bucle
+            if fun(x1) * fun(x2) <= 0
                 return
             else
+             % Sino el intervalo avanza dx, para ello le
+             % damos a x1 el valor del anterior x2 y a x2 le sumamos dx.
                 [x1, x2] = deal(x2, x2+dx);
             end
         end
+        % Si se ha encontrado la raíz en el subintervalo y x2 esta 
+        % fuera del intervalo se le asigna b 
         if x2 ~= b
             x2 = b;
         end
+    % Si no se encuentra ninguna raíz le damos el valor NaN a 
+    % los límites del  intervalo
     else
          [x1, x2] = deal(NaN);
     end
